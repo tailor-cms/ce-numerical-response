18dfe7ebb4ebf2753477dfb9a0fa9ec092c22485
rkusan00 <renato.kusanovic@gmail.com>
2024-10-07 16:06:20 +0200

    Implement display component

--- packages/display/src/components/Display.vue
+++ packages/display/src/components/Display.vue
@@ -1,33 +1,83 @@
 <template>
-  <div class="tce-root">
-    <p>This is the Display version of the content element id: {{ id }}</p>
-    <div class="mt-6 mb-2">
-      Counter:
-      <span class="font-weight-bold">{{ data.count }}</span>
+  <VForm ref="form" class="tce-root" @submit.prevent="submit">
+    <div class="px-2 my-4">{{ data.question }}</div>
+    <VTextField
+      v-for="([prefix, sufix], index) in items"
+      :key="index"
+      v-model="response[index]"
+      :prefix="prefix"
+      :readonly="submitted"
+      :rules="[requiredRule]"
+      :suffix="sufix"
+      class="my-3"
+      label="Answer"
+    >
+      <template v-if="submitted" #append>
+        <VIcon v-bind="iconProps(index)" />
+      </template>
+    </VTextField>
+    <VAlert
+      v-if="submitted"
+      :text="userState?.isCorrect ? 'Correct' : 'Incorrect'"
+      :type="userState?.isCorrect ? 'success' : 'error'"
+      class="mb-3"
+      rounded="lg"
+      variant="tonal"
+    />
+    <div class="d-flex justify-end">
+      <VBtn v-if="!submitted" type="submit" variant="tonal">Submit</VBtn>
+      <VBtn v-else variant="tonal" @click="submitted = false">Try Again</VBtn>
     </div>
-    <v-btn class="my-6" @click="submit">Update user state</v-btn>
-    <div>
-      <div class="mb-1 text-subtitle-2">User state:</div>
-      <pre class="text-body-2">{{ userState }}</pre>
-    </div>
-  </div>
+  </VForm>
 </template>
 
 <script setup lang="ts">
+import { computed, ref, watch } from 'vue';
+import cloneDeep from 'lodash/cloneDeep';
 import { ElementData } from '@tailor-cms/ce-numerical-response-manifest';
+import zip from 'lodash/zip';
+
+const initializeResponse = () =>
+  cloneDeep(props.userState?.response) ??
+  Array(props.data.prefixes.length).fill('');
 
 const props = defineProps<{ id: number; data: ElementData; userState: any }>();
 const emit = defineEmits(['interaction']);
 
-const submit = () => emit('interaction', { id: props.id });
+const form = ref<HTMLFormElement>();
+const submitted = ref('isSubmitted' in (props.userState ?? {}));
+const response = ref<string[]>(initializeResponse());
+
+const items = computed(() => zip(props.data.prefixes, props.data.suffixes));
+
+const submit = async () => {
+  const { valid } = await form.value?.validate();
+  if (valid) emit('interaction', { response: response.value });
+};
+
+const requiredRule = (val: string | boolean | number) => {
+  return !!val || 'You have to enter your answer.';
+};
+
+const iconProps = (index: number) => {
+  const { response, correct } = props.userState;
+  const isCorrect = response?.[index] === correct?.[index];
+  if (isCorrect) return { icon: 'mdi-check-circle', color: 'success' };
+  return { icon: 'mdi-close-circle', color: 'error' };
+};
+
+watch(
+  () => props.userState,
+  (state = {}) => {
+    response.value = initializeResponse();
+    submitted.value = 'isCorrect' in state;
+  },
+  { deep: true },
+);
 </script>
 
 <style scoped>
 .tce-root {
-  background-color: transparent;
-  margin-top: 1rem;
-  padding: 1.25rem;
-  border: 2px dashed #888;
   font-family: Arial, Helvetica, sans-serif;
   font-size: 1rem;
 }
